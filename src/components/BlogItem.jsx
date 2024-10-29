1c810d24c7b053e5cc7aad725b1d085c4721440f
rar34 <ra.cse34@gmail.com>
2024-10-30 00:22:52 +0600

    filter blog item according to category

--- src/components/BlogItem.jsx
+++ src/components/BlogItem.jsx
@@ -6,7 +6,7 @@ import { FaArrowRight } from "react-icons/fa";
 const BlogItem = ({title, description, category, image}) => {
     return (
         <div className='container mx-auto'>
-            <div className='min-h-[470px] border border-black hover:shadow-[-7px_7px_0px_#000]'>
+            <div className='min-h-[500px] border border-black hover:shadow-[-7px_7px_0px_#000]'>
                 <Image src={image} width={300} alt='' height={200} className='w-full' />
                 <p className='text-sm bg-black text-white ml-5 mt-5 px-1 inline-block'>{category}</p>
                 <div className='p-5'>
--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,20 +1,23 @@
 import { blog_data } from '@/assets/assets';
-import React from 'react';
+import React, { useState } from 'react';
 import BlogItem from './BlogItem';
 
 const BlogList = () => {
+
+    const [menu, setMenu] = useState("All");
+
     return (
         <div className='container mx-auto my-24'>
             <div className='flex justify-center my-10 gap-6'>
-                <button className='bg-black px-4 py-1 rounded-sm text-white'>All</button>
-                <button>Technology</button>
-                <button>Startup</button>
-                <button>Lifestyle</button>
+                <button onClick={() => setMenu('All')} className={menu === "All" ? 'bg-black px-4 py-1 rounded-sm text-white' : ""}>All</button>
+                <button onClick={() => setMenu('Technology')} className={menu === "Technology" ? 'bg-black px-4 py-1 rounded-sm text-white' : ""}>Technology</button>
+                <button onClick={() => setMenu('Startup')} className={menu === "Startup" ? 'bg-black px-4 py-1 rounded-sm text-white' : ""}>Startup</button>
+                <button onClick={() => setMenu('Lifestyle')} className={menu === "Lifestyle" ? 'bg-black px-4 py-1 rounded-sm text-white' : ""}>Lifestyle</button>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
-                    blog_data.map(item =>{
-                        return <BlogItem key={item.id} image={item.image} title={item.title} description={item.description} />
+                    blog_data.filter((item) => menu === "All" ? true : item.category === menu).map(item => {
+                        return <BlogItem key={item.id} image={item.image} title={item.title} description={item.description} category={item.category} />
                     })
                 }
             </div>
