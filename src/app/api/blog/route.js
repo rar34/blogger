ce5d1ef651fa28ec8890011e46c42b9c73bb3475
rar34 <ra.cse34@gmail.com>
2024-11-06 21:22:45 +0600

    make a function to store image

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -11,3 +11,9 @@ export async function GET(request) {
 
     return NextResponse.json({message: 'API Working...'})
 }
+
+export async function POST(request) {
+    const formData = await request.formData();
+    const timeStamp = Date.now();
+    const image = formData.get('image')
+}
