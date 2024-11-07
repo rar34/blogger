fde5d5c3c60581205cf0781581ce9ab6d713ed58
rar34 <ra.cse34@gmail.com>
2024-11-07 22:36:33 +0600

    save photo in public folder.

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -1,7 +1,8 @@
 import { connectDB } from "@/lib/config/db";
 import { NextResponse } from "next/server";
+import { writeFile } from 'fs/promises'
 
-const LoadDb = async ()=>{
+const LoadDb = async () => {
     await connectDB();
 }
 
@@ -9,11 +10,19 @@ LoadDb();
 
 export async function GET(request) {
 
-    return NextResponse.json({message: 'API Working...'})
+    return NextResponse.json({ message: 'API Working...' })
 }
 
 export async function POST(request) {
     const formData = await request.formData();
     const timeStamp = Date.now();
-    const image = formData.get('image')
+    const image = formData.get('image');
+
+    const imageByteData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByteData);
+    const path = `./public/${timeStamp}_${image.name}`;
+    await writeFile(path, buffer);
+    const imgUrl = `/${timeStamp}_${image.name}`;
+    console.log(imgUrl);
+    return NextResponse.json({ imgUrl })
 }
