7a2ad9e74f0a291a77350398c84c80b30459d21e
rar34 <ra.cse34@gmail.com>
2024-11-13 22:55:22 +0600

    added all blogs components

---
+++ src/app/allblogs/page.jsx
@@ -0,0 +1,16 @@
+import Navbar from '@/components/Navbar';
+import React from 'react';
+
+const AllPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className='container mx-auto my-14'>
+
+                All blogs will displays here.........
+            </div>
+        </div>
+    );
+};
+
+export default AllPage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         },
         {
             title: "All Blogs",
-            path: "/all-blogs"
+            path: "/allblogs"
         },
     ]
 
